a329fd2ad3ab84b00f145263423ee41b0ddf3751
Kevin Kanyingi <kevin.kanyingi@clearscore.com>
2026-05-19 12:47:29 +0100

    chore: add CI origin to valid origins
    DATA1-3415

--- packages/embedded-connection-journey/src/lib/constants.ts
+++ packages/embedded-connection-journey/src/lib/constants.ts
@@ -1,1 +1,5 @@
-export const DATA_ONE_VALID_ORIGINS = ['https://connect.data.one', 'https://connect-sandbox.data.one'];
+export const DATA_ONE_VALID_ORIGINS = [
+    'https://connect.data.one',
+    'https://connect-sandbox.data.one',
+    'https://admin.data-one-user-services-ci.eu-west-2.dev.clearscore.io',
+];
